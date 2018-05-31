e608aa8cab2f049c67115eb6bc12cd1514f28dd6
troychuinard <tccpg288@gmail.com>
2018-05-31 18:52:17 -0500

    Updating Spinner
    Updating Spinner to reflect popular movies

--- app/src/main/java/popularmovies/troychuinard/com/popularmovies/MainActivity.java
+++ app/src/main/java/popularmovies/troychuinard/com/popularmovies/MainActivity.java
@@ -2,6 +2,7 @@ package popularmovies.troychuinard.com.popularmovies;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,9 +11,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.squareup.picasso.Picasso;
 
@@ -30,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_spinner, menu);
+        MenuItem item = menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) item.getActionView();
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.spiner_list_item_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        return true;
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -44,70 +61,73 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-
-
-            // Provide a reference to the views for each data item
-            // Complex data items may need more than one view per item, and
-            // you provide access to all the views for a data item in a view holder
-            public class ViewHolder extends RecyclerView.ViewHolder {
-                // each data item is just a string in this case
-                protected ImageView mResultImage;
-
-
-                public ViewHolder(View v) {
-                    super(v);
-                    mResultImage = (ImageView) v.findViewById(R.id.movie_poster_image);
-
-
-                }
-            }
-
-            // Provide a suitable constructor (depends on the kind of dataset)
-            public MyAdapter(ArrayList<String> mDataSet) {
-                mMovieURLS = mDataSet;
-            }
-
-
-            // Create new views (invoked by the layout manager)
-            @Override
-            public ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                 int viewType) {
-                // create a new view
-                View v = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.grid_item, parent, false);
-                // set the view's size, margins, paddings and layout parameters
-                return new ViewHolder(v);
-            }
-
-            // Replace the contents of a view (invoked by the layout manager)
-
-            //The OutOfBoundsException is pointing here
-            @Override
-            public void onBindViewHolder(ViewHolder holder, final int position) {
-                Log.v("ON_BIND", "ON_BINDVIEWHOLDER CALLED");
-                final String urlForPhoto = mMovieURLS.get(position);
-                if (mProgressBar.getVisibility() == View.VISIBLE) {
-                    mProgressBar.setVisibility(View.INVISIBLE);
-                }
-                Picasso.with(getApplicationContext())
-                        .load(urlForPhoto)
-                        .placeholder(R.drawable.progress_animation)
-                        .into(holder.mResultImage);
-
-
-                });
-
-            }
-            //test
-
-            //test
-            // Return the size of your dataset (invoked by the layout manager)
-            @Override
-            public int getItemCount() {
-                return mMovieURLS.size();
-            }
-        }
+//        public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+//
+//
+//            // Provide a reference to the views for each data item
+//            // Complex data items may need more than one view per item, and
+//            // you provide access to all the views for a data item in a view holder
+//            public class ViewHolder extends RecyclerView.ViewHolder {
+//                // each data item is just a string in this case
+//                protected ImageView mResultImage;
+//
+//
+//                public ViewHolder(View v) {
+//                    super(v);
+//                    mResultImage = (ImageView) v.findViewById(R.id.movie_poster_image);
+//
+//
+//                }
+//            }
+//
+//
+//
+//
+//            // Provide a suitable constructor (depends on the kind of dataset)
+//            public MyAdapter(ArrayList<String> mDataSet) {
+//                mMovieURLS = mDataSet;
+//            }
+//
+//
+//            // Create new views (invoked by the layout manager)
+//            @Override
+//            public ViewHolder onCreateViewHolder(ViewGroup parent,
+//                                                 int viewType) {
+//                // create a new view
+//                View v = LayoutInflater.from(parent.getContext())
+//                        .inflate(R.layout.grid_item, parent, false);
+//                // set the view's size, margins, paddings and layout parameters
+//                return new ViewHolder(v);
+//            }
+//
+//            // Replace the contents of a view (invoked by the layout manager)
+//
+//            //The OutOfBoundsException is pointing here
+//            @Override
+//            public void onBindViewHolder(ViewHolder holder, final int position) {
+//                Log.v("ON_BIND", "ON_BINDVIEWHOLDER CALLED");
+//                final String urlForPhoto = mMovieURLS.get(position);
+//                if (mProgressBar.getVisibility() == View.VISIBLE) {
+//                    mProgressBar.setVisibility(View.INVISIBLE);
+//                }
+//                Picasso.with(getApplicationContext())
+//                        .load(urlForPhoto)
+//                        .placeholder(R.drawable.progress_animation)
+//                        .into(holder.mResultImage);
+//
+//
+//                });
+//
+//            }
+//            //test
+//
+//            //test
+//            // Return the size of your dataset (invoked by the layout manager)
+//            @Override
+//            public int getItemCount() {
+//                return mMovieURLS.size();
+//            }
+//        }
 
 
     }
